230dfee9ea7a4b1bba46b07867a47a010628503f
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2023-12-05 23:19:47 +0200

    Update settings.py

--- kursova/settings.py
+++ kursova/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hq&$#e@9w1^(48+)vr!w3uo#1dnw-hx^(1k#lzb#5259k5*wz$'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
