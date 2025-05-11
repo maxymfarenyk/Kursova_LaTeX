aa6282a30e09dd0c9cbfdea39a964667552d8c07
maxymfarenyk <maxfar2004@gmail.com>
2025-05-11 14:38:06 +0300

    updated settings.py

--- kursova/settings.py
+++ kursova/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'latexfiles.onrender.com']
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
